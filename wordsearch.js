c64085d643d4974711f928caa5bf3e9cb4bf50a0
Rebecca Benedict Anton <rebecca.benedictanton@gmail.com>
2023-07-26 01:38:58 +0000

    Pair programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = function(matrix, word) {
+  let answer = false;
+  let horizontal = matrix.map(ls => ls.join(''));
 
-module.exports = wordSearch
+  for (const index of horizontal) {
+    if (index.match(word)) {
+      answer = true;
+    }
+  }
+};
+  if (!answer) {
+    let result = [];
+    for (let j = 0; j < matrix.length; j++) {
+      for (let i = 0; i < matrix[j].length; i++) {
+        if (!Array.isArray(result[i])) {
+          result[i] = [];
+        }
+        result[i][j] = matrix[j][i];
+      }
+    }
+    let vertical = result.map(ls => ls.join(''));
+    for (const index of vertical) {
+      if (index.match(word)) {
+        answer = true;
+      }
+    }
+  };
